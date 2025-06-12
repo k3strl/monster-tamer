be7610d079c287cd0d6a1218edd09d7ed4ab7fe7
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-06-12 12:21:07 -0500

    initial commit

---
+++ src/scenes/preload-scene.js
@@ -0,0 +1,69 @@
+import { BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, HEALTH_BAR_ASSET_KEYS, MONSTER_ASSET_KEYS } from "../assets/asset-keys.js";
+import Phaser from "../lib/phaser.js";
+import { SCENE_KEYS } from "./scene-keys.js";
+
+export class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: SCENE_KEYS.PRELOAD_SCENE,
+    });
+    console.log(SCENE_KEYS.PRELOAD_SCENE);
+  }
+
+  init() {
+    //  The initialization state; the scene initialized first. This is where we place code we want to run every time the scene is started. For example, code for a score mechanic might live here, as we would want it to reset to 0 every time the scene starts. After this code runs, Phaser will advance into the preload state/method.
+    console.log('init'); 
+  }
+
+  preload() {
+    // Used for game assets. Phaser will only move on in the queue once all assets have finished loading. As such, this can take a long time if loading in lots of large files.
+    const monsterTamerAssetPath = 'assets/images/monster-tamer';
+    const kenneysAssetPath = 'assets/images/kenneys-assets';
+
+    // battle backgrounds
+    this.load.image(
+      BATTLE_BACKGROUND_ASSET_KEYS.FOREST,
+      `${monsterTamerAssetPath}/battle-backgrounds/forest-background.png`
+    );
+
+    // battle assets
+    this.load.image(
+      BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND,
+      `${kenneysAssetPath}/ui-space-expansion/custom-ui.png`
+    );
+
+    // health bar assets
+    this.load.image(
+      HEALTH_BAR_ASSET_KEYS.RIGHT_CAP,
+      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_right.png`
+    );
+    this.load.image(
+      HEALTH_BAR_ASSET_KEYS.MIDDLE,
+      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_mid.png`
+    );
+    this.load.image(
+      HEALTH_BAR_ASSET_KEYS.LEFT_CAP,
+      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`
+    );
+
+    // monster assets
+    this.load.image(
+      MONSTER_ASSET_KEYS.CARNODUSK,
+      `${monsterTamerAssetPath}/monsters/carnodusk.png`
+    );
+    this.load.image(
+      MONSTER_ASSET_KEYS.IGUANIGNITE,
+      `${monsterTamerAssetPath}/monsters/iguanignite.png`
+    );
+  }
+
+  create() {
+    // This is where you can start creating objects and placing them into your scene. I.e., a background image being rendered out to the canvas, for example.
+    this.add.image(0, 0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
+  }
+
+  update() {
+    // Code you want to run every tick, or frame update, inside the internal engine. This is where you can do things like listen for player input, or update your game objects based on physics data.
+    console.log('update');
+  }
+}
