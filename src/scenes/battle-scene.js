f2d1e67e3b3cc90443b12809aa41bcab612ee127
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-06-12 17:07:51 -0500

    fixed HP bar bugs

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -14,19 +14,17 @@ export class BattleScene extends Phaser.Scene {
     });
   }
 
-  init() {}
-
   create() {
     console.log(`[${BattleScene.name}:create] invoked`);
     // create main background
     // order matters; Phaser will render things in the order we tell it to. If we render the monsters first, the background will be set over TOP of the monsters, which is not what we want. .setDepth can override this default behavior.
     this.add.image(0, 0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
 
-    // render out player and enemy monsters
+    // render out player & enemy monsters
     this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0);
     this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
 
-    //render out player monster name & health bar
+    //render out PLAYER monster name & health bar
     const playerMonsterName = this.add.text(
       30,
       20,
@@ -41,19 +39,53 @@ export class BattleScene extends Phaser.Scene {
         .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
         .setOrigin(0),
       playerMonsterName,
-      this.#createHealth(34, 34),
+      this.#createHealthBar(34, 34),
       this.add.text(playerMonsterName.width + 35, 23, 'L5', {
         color: '#ED474B',
         fontSize: '28px',
       }),
-      this.add.text(playerMonsterName.width + 30, 55, 'HP', {
+      this.add.text(30, 55, 'HP', {
+        color: '#FF6505',
+        fontSize: '24px',
+        fontStyle: 'italic',
+      }),
+      this.add.text(443, 80, '25/25', {
+        color: '#7E3D3F',
+        fontSize: '16px',
+      })
+      .setOrigin(1, 0),
+    ]);
+
+    //render out ENEMY monster name & health bar
+    const enemyMonsterName = this.add.text(
+      30,
+      20,
+      MONSTER_ASSET_KEYS.CARNODUSK,
+      {
+        color: '#7E3D3F',
+        fontSize: '32px',
+      }
+    );
+    this.add.container(0, 0, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0)
+        .setScale(1, 0.8),
+      enemyMonsterName,
+      this.#createHealthBar(34, 34),
+      this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
         color: '#ED474B',
         fontSize: '28px',
+      }),
+      this.add.text(30, 55, 'HP', {
+        color: '#FF6505',
+        fontSize: '24px',
+        fontStyle: 'italic',
       }),
     ]);
   }
 
-  #createHealth(x, y) {
+  #createHealthBar(x, y) {
     const scaleY = 0.7;
     const leftCap = this.add
       .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
@@ -61,13 +93,14 @@ export class BattleScene extends Phaser.Scene {
       .setScale(1, scaleY);
     const middle = this.add
       .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
-      .setOrigin(0, 0.5);
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
     middle.displayWidth = 360;
     const rightCap = this.add
       .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
-      .setOrigin(0, 0.5);
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
+
     return this.add.container(x, y, [leftCap, middle, rightCap]);
   }
-
-  update() {}
 }
